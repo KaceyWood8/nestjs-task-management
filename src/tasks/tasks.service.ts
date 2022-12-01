fd37cdbd978713b6c786df93a4d3c4db2edd2f5d
kaceywood8 <kacey.l.wood@ey.com>
2022-12-01 17:06:22 -0500

    Delete a Task

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -45,14 +45,18 @@ export class TasksService {
 
     return found;
   }
+
   createTask(createTaskDto: CreateTaskDto): Promise<Task> {
     return this.tasksRepository.createTask(createTaskDto);
   }
 
-  // deleteTask(id: string): void {
-  //   const found = this.getTaskByID(id);
-  //   this.tasks = this.tasks.filter((tasks) => tasks.id !== found.id);
-  // }
+  async deleteTask(id: string): Promise<void> {
+    const result = await this.tasksRepository.delete(id);
+
+    if (result.affected === 0) {
+      throw new NotFoundException(`Task with ID "${id}" not found`);
+    }
+  }
   // updateTaskStatus(id: string, status: TaskStatus) {
   //   const task = this.getTaskByID(id);
   //   task.status = status;
